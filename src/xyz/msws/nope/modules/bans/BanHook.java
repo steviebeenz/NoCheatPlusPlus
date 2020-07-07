e49b6196061088a31eb36b7bce561021d7a19a50
Restyled.io <commits@restyled.io>
2020-07-07 00:04:39 +0000

    Restyled by clang-format

--- src/xyz/msws/nope/modules/bans/BanHook.java
+++ src/xyz/msws/nope/modules/bans/BanHook.java
@@ -1,39 +1,29 @@
 package xyz.msws.nope.modules.bans;
 
 import java.util.UUID;
-
 import xyz.msws.nope.NOPE;
 import xyz.msws.nope.modules.AbstractModule;
 
 /**
  * Used to register bans in ban plugins.
- * 
+ *
  * @author steviebeenz
  *
  */
 public abstract class BanHook extends AbstractModule {
-	public BanHook(NOPE plugin) {
-		super(plugin);
-	}
+  public BanHook(NOPE plugin) { super(plugin); }
 
-	@Override
-	public void enable() {
-	}
+  @Override
+  public void enable() {}
 
-	@Override
-	public void disable() {
-	}
+  @Override
+  public void disable() {}
 
-	public void ban(UUID player) {
-		ban(player, null);
-	}
+  public void ban(UUID player) { ban(player, null); }
 
-	public void ban(UUID player, String reason) {
-		ban(player, reason, -1);
-	}
+  public void ban(UUID player, String reason) { ban(player, reason, -1); }
 
-	public abstract void ban(UUID player, String reason, long time);
+  public abstract void ban(UUID player, String reason, long time);
 
-	public abstract int bans(UUID player);
-
+  public abstract int bans(UUID player);
 }
